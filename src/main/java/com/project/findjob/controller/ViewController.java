b9eba18196c526c8468389230c76a9f5079637a9
YoonHanmin <gksals8473@naver.com>
2024-02-20 23:32:03 +0900

    JWT 구현

--- src/main/java/com/project/findjob/controller/ViewController.java
+++ src/main/java/com/project/findjob/controller/ViewController.java
@@ -5,10 +5,14 @@ import com.project.findjob.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Optional;
 
 @Controller
@@ -38,14 +42,29 @@ public class ViewController {
         return "user/profile";
            }
 
+    @GetMapping("/")
+    @ResponseBody
+    public String mainP() {
+
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        log.info("@# 응답은 ==>"+role+name);
+        return "Main Controller : "+name;
+    }
+
     @GetMapping("/main_owner")
     public String mainowener(){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        log.info("@@# getName() ==>"+auth.getName());
-        Optional<User> user = userRepository.findByUserid(auth.getName());
-        User my = user.get();
-        log.info("roles ==>"+my.getRoles());
-        log.info("@#@# getAuthorise() ===> "+auth.getAuthorities());
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        log.info("@@# getName() ==>"+auth.getName());
+//        Optional<User> user = userRepository.findByUserid(auth.getName());
+//        User my = user.get();
+//        log.info("roles ==>"+my.getRoles());
+//        log.info("@#@# getAuthorise() ===> "+auth.getAuthorities());
 
         return "main_owner";
     }
